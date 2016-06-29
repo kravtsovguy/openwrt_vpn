cf0494766788a037da8fa6ced83aeb061a884abc
Matvey Kravtsov <matvey@MBP-Matvey.lan>
2016-06-29 20:20:57 +0300

    v1.3

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express         = require('express');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
+application.use("/public", express.static(path.join(__dirname, 'public')));
 
 var firebase = require("firebase");
 var config = {
