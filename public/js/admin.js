66fd93ce6a1f054a4eed70c762f11655b5465563
Matvey Kravtsov <matvey@MBP-Matvey.lan>
2016-07-07 06:32:12 +0300

    v1.8

---
+++ public/js/admin.js
@@ -0,0 +1,25 @@
+var app = angular.module('myApp', []);
+app.controller('myCtrl', function($scope, $location, $http) {
+    $scope.myUrl = $location.absUrl();
+    
+    $scope.findUser = function(){
+        $http.get("/api/user/"+$scope.mac)
+        .then(function(response) {
+            $scope.user = response.data;
+            $scope.nuser = angular.copy($scope.user);
+        });
+    }
+    
+    $scope.saveUser = function(){
+        $http.put("/api/user/"+$scope.mac, $scope.nuser)
+        .then(
+            function(response){
+                //alert(JSON.stringify(response.data));
+                location.reload();
+            }, 
+            function(response){
+
+            }
+        );
+    }
+});
--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 "use strict";
 
+var http = require('http');
+
+var bodyParser = require('body-parser')
 var express = require('express');
 var app = express();
 
+app.use(bodyParser.json())
+
+var request = require('request');
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
@@ -23,22 +29,58 @@ var db = firebase.database();
 	
 app.get('/vpn/:mac', function (req, res) {
     
+    var str = 'http://localhost:'+server.address().port+'/api/user/'+req.params.mac;
+    request(str, function (error, response, body) {
+        var v = JSON.parse(body);
+        res.send(v.login + "\n" + v.password);
+    });
+});
+
+app.get('/api/user/:mac', function (req, res) {
+    
 	var ref = db.ref("users/" + req.params.mac);
 	ref.once("value", function(snapshot) {
 		var v = snapshot.val();
-        if(v!=null)
-            res.send(v.login + "\n" + v.password);
-        else
-            res.send("empty\nempty");
-        
-		console.log('Requested '+JSON.stringify(v)+' for '+req.params.mac);
-	});	
+        var user = {};
+        user.login = v != null ? v.login : "empty";
+        user.password = v != null ? v.password: "empty";
+        res.json(user);
+        console.log('Requested '+JSON.stringify(v)+' for '+req.params.mac);
+    });	
+});
+
+app.put('/api/user/:mac', function (req, res) {
+    var auth = req.body;
+    console.log("New data");
+    console.log(req.params.mac+' : '+JSON.stringify(auth));
+    var ref = db.ref("users/" + req.params.mac);
+    ref.set(auth,function(error) {
+        var r = {};
+        if (error) {
+            r.result = "ERROR";
+        } else {
+            r.result = "OK";
+        }
+        res.json(r);
+    });
 });
 
 app.get('/', function (req, res) {
-    res.render('index', { title: 'Hey'});
+    res.render('index', { title: 'VPN'});
+});
+app.get('/pay', function (req, res) {
+    res.render('pay');
 });
 
-app.listen(app.get('port'), function(){
+app.get('/admin', function (req, res) {
+    res.render('admin');
+});
+
+app.use(function(req, res){
+   //res.sendStatus(404);
+    res.redirect('/');
+});
+
+var server = app.listen(app.get('port'), function(){
     console.log('Node app is running on port', app.get('port'));
 });
